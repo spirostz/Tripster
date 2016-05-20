5f7a04060567c55af1054892701d19e0f2d84cc4
spirostz <stzoras@tripsta.com>
2016-05-20 17:32:41 +0300

    Shortest path exposed

--- src/main/java/spacepi/graphs/CreateObjectsMain.java
+++ src/main/java/spacepi/graphs/CreateObjectsMain.java
@@ -1,18 +1,11 @@
 package spacepi.graphs;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import com.google.gson.Gson;
 
 import spacepi.model.map.RouteInitial;
-import spacepi.model.map.RouteMap;
-import spacepi.model.map.RoutePoint;
-import spacepi.model.map.RouteReference;
+import spacepi.movement.PathHelper;
 
 public class CreateObjectsMain {
 
@@ -23,50 +16,14 @@ public class CreateObjectsMain {
 		RouteInitial routeInitial = gson.fromJson(CreatePointModel.getSampleMapAsJson(), RouteInitial.class);
 
 		//System.out.println(gson.toJson(routeInitial));
-		System.out.println(gson.toJson(dijkstra(routeInitial.getRouteMaps().get("testMap"), "P1")));
+		//System.out.println(gson.toJson(PathHelper.dijkstra(routeInitial.getRouteMaps().get("testMap"), "P1")));
+		System.out.println(PathHelper.shortestPath(routeInitial.getRouteMaps().get("testMap"),  "P4", "P3"));
+		
 	}
 
-	public static Map<RoutePoint, Double> dijkstra(RouteMap routeMap, String initialPointId) {
-
-		int nodeCount = routeMap.getRoutePoints().size();
-
-		List<RoutePoint> exploredPoints = new ArrayList<>();
-		exploredPoints.add(routeMap.getRoutePoints().get(initialPointId));
-
-		Map<RoutePoint, Double> distancesMap = new HashMap<>();
-		distancesMap.put(routeMap.getRoutePoints().get(initialPointId), 0.0);
-
-		Map<String, String> previous = new HashMap<>();
-		
-		while (nodeCount != exploredPoints.size()) {
-			double minimum = Double.POSITIVE_INFINITY;
-			RoutePoint tempPoint = null;
-			RoutePoint tempPrevious = null;
-
-			for (RoutePoint routePoint : exploredPoints) {
-				for (Entry<String, RouteReference> entry : routePoint.getReferencePoints().entrySet()) {
-
-					RoutePoint point = routeMap.getRoutePoints().get(entry.getKey());
-					double distance = entry.getValue().getDistance();
-
-					if (!exploredPoints.contains(point)) {
-						if (distancesMap.get(routePoint) + distance < minimum) {
-							minimum = distancesMap.get(routePoint) + distance;
-							tempPoint = point;
-							tempPrevious = routePoint;
-						}
-					}
-				}
-			}
-			
-			previous.put(tempPoint.getUniqueId(), tempPrevious.getUniqueId());
-			exploredPoints.add(tempPoint);
-			distancesMap.put(tempPoint, minimum);
-		}
-
-		System.out.println(gson.toJson(previous));
-		
-		return distancesMap;
-	}
+	
+	
+	
+	
 
 }
--- src/main/java/spacepi/model/map/dijkstra/RoutePointMetadata.java
+++ src/main/java/spacepi/model/map/dijkstra/RoutePointMetadata.java
@@ -1,5 +1,69 @@
 package spacepi.model.map.dijkstra;
 
-public class DijkstraMetadata {
+import java.io.Serializable;
+
+public class RoutePointMetadata implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6727167552210348199L;
+
+	String currentPointId;
+
+	String initilPointId;
+	String previousPointId;
+	double distanceFromInitial;
+
+	public RoutePointMetadata() {
+		super();
+	}
+
+	public RoutePointMetadata(String currentPointId, String initilPointId, String previousPointId,
+			double distanceFromInitial) {
+		super();
+		this.currentPointId = currentPointId;
+		this.initilPointId = initilPointId;
+		this.previousPointId = previousPointId;
+		this.distanceFromInitial = distanceFromInitial;
+	}
+
+	public String getCurrentPointId() {
+		return currentPointId;
+	}
+
+	public void setCurrentPointId(String currentPointId) {
+		this.currentPointId = currentPointId;
+	}
+
+	public String getInitilPointId() {
+		return initilPointId;
+	}
+
+	public void setInitilPointId(String initilPointId) {
+		this.initilPointId = initilPointId;
+	}
+
+	public String getPreviousPointId() {
+		return previousPointId;
+	}
+
+	public void setPreviousPointId(String previousPointId) {
+		this.previousPointId = previousPointId;
+	}
+
+	public double getDistanceFromInitial() {
+		return distanceFromInitial;
+	}
+
+	public void setDistanceFromInitial(double distanceFromInitial) {
+		this.distanceFromInitial = distanceFromInitial;
+	}
+
+	@Override
+	public String toString() {
+		return "RoutePointMetadata [currentPointId=" + currentPointId + ", initilPointId=" + initilPointId
+				+ ", previousPointId=" + previousPointId + ", distanceFromInitial=" + distanceFromInitial + "]";
+	}
 
 }
