cc5ee8789fa1dd3c8a8f0ec80983ab46c4f13292
spirostz <stzoras@tripsta.com>
2016-05-20 18:38:52 +0300

    Shortest path with directions ready for Tripster

--- src/main/java/spacepi/graphs/CreateObjectsMain.java
+++ src/main/java/spacepi/graphs/CreateObjectsMain.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import com.google.gson.Gson;
 
 import spacepi.model.map.RouteInitial;
+import spacepi.model.map.enums.RouteDirectionType;
 import spacepi.movement.PathHelper;
 
 public class CreateObjectsMain {
@@ -17,7 +18,8 @@ public class CreateObjectsMain {
 
 		//System.out.println(gson.toJson(routeInitial));
 		//System.out.println(gson.toJson(PathHelper.dijkstra(routeInitial.getRouteMaps().get("testMap"), "P1")));
-		System.out.println(PathHelper.shortestPath(routeInitial.getRouteMaps().get("testMap"),  "P4", "P3"));
+		//System.out.println(PathHelper.shortestPath(routeInitial.getRouteMaps().get("testMap"),  "P4", "P3"));
+		System.out.println(PathHelper.movementRoute(routeInitial.getRouteMaps().get("testMap"),  "P4", "P3", RouteDirectionType.NORTH));
 		
 	}
 
